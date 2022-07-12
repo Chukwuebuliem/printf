49e8ec7a52e2bcee8c8205ece35ba06d25d68c5a
Chukwuebuliem <chukwuebuliemsuccess@gmail.com>
2022-07-12 13:43:52 -0700

    function that produces output according to a format

--- _printf.c
+++ _printf.c
@@ -18,7 +18,6 @@ int _printf(const char *format, ...)
 	register int count = 0;
 
 	va_start(arguments, format);
-
 	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
